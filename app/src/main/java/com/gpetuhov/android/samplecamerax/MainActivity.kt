417bac68f4ed4d03e568009a64bab0f835a87ea4
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-07-26 13:20:09 +0300

    Refactor code

--- app/src/main/java/com/gpetuhov/android/samplecamerax/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplecamerax/MainActivity.kt
@@ -8,13 +8,13 @@ import android.os.Bundle
 import android.util.Rational
 import android.util.Size
 import android.view.Surface
+import android.view.TextureView
 import android.view.ViewGroup
 import androidx.camera.core.CameraX
 import androidx.camera.core.Preview
 import androidx.camera.core.PreviewConfig
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
@@ -26,22 +26,26 @@ class MainActivity : AppCompatActivity() {
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     }
 
+    private lateinit var viewFinder: TextureView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewFinder = findViewById(R.id.view_finder)
 
         // Request camera permissions
         if (allPermissionsGranted()) {
             // Instead of calling `startCamera()` on the main thread, we use viewFinder.post { ... }
             // to make sure that `viewFinder` has already been inflated into the view
             // when startCamera() is called.
-            view_finder.post { startCamera() }
+            viewFinder.post { startCamera() }
         } else {
             ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
 
         // Every time the provided texture view changes, recompute layout
-        view_finder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+        viewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
             updateTransform()
         }
     }
@@ -63,11 +67,11 @@ class MainActivity : AppCompatActivity() {
         preview.setOnPreviewOutputUpdateListener {
 
             // To update the SurfaceTexture, we have to remove it and re-add it
-            val parent = view_finder.parent as ViewGroup
-            parent.removeView(view_finder)
-            parent.addView(view_finder, 0)
+            val parent = viewFinder.parent as ViewGroup
+            parent.removeView(viewFinder)
+            parent.addView(viewFinder, 0)
 
-            view_finder.surfaceTexture = it.surfaceTexture
+            viewFinder.surfaceTexture = it.surfaceTexture
             updateTransform()
         }
 
@@ -82,11 +86,11 @@ class MainActivity : AppCompatActivity() {
         val matrix = Matrix()
 
         // Compute the center of the view finder
-        val centerX = view_finder.width / 2f
-        val centerY = view_finder.height / 2f
+        val centerX = viewFinder.width / 2f
+        val centerY = viewFinder.height / 2f
 
         // Correct preview output to account for display rotation
-        val rotationDegrees = when(view_finder.display.rotation) {
+        val rotationDegrees = when(viewFinder.display.rotation) {
             Surface.ROTATION_0 -> 0
             Surface.ROTATION_90 -> 90
             Surface.ROTATION_180 -> 180
@@ -96,7 +100,7 @@ class MainActivity : AppCompatActivity() {
         matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
 
         // Finally, apply transformations to our TextureView
-        view_finder.setTransform(matrix)
+        viewFinder.setTransform(matrix)
     }
 
     // Process result from permission request dialog box
@@ -104,7 +108,7 @@ class MainActivity : AppCompatActivity() {
         requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
             if (allPermissionsGranted()) {
-                view_finder.post { startCamera() }
+                viewFinder.post { startCamera() }
             } else {
                 toast("Permissions not granted by the user.")
                 finish()
