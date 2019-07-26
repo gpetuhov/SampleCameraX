a3118932b569907cfef9e32618f1c9a47e729d5e
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-07-26 12:34:20 +0300

    Request camera permission

--- app/src/main/java/com/gpetuhov/android/samplecamerax/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplecamerax/MainActivity.kt
@@ -1,12 +1,68 @@
 package com.gpetuhov.android.samplecamerax
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 10
+
+        // This is an array of all the permission specified in the manifest
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            // Instead of calling `startCamera()` on the main thread, we use viewFinder.post { ... }
+            // to make sure that `viewFinder` has already been inflated into the view
+            // when startCamera() is called.
+            view_finder.post { startCamera() }
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        // Every time the provided texture view changes, recompute layout
+        view_finder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+            updateTransform()
+        }
+    }
+
+    // === Private methods ===
+
+    private fun startCamera() {
+        // TODO: Implement CameraX operations
+    }
+
+    private fun updateTransform() {
+        // TODO: Implement camera viewfinder transformations
+    }
+
+    // Process result from permission request dialog box
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                view_finder.post { startCamera() }
+            } else {
+                toast("Permissions not granted by the user.")
+                finish()
+            }
+        }
+    }
+
+    // Check if all permission specified in the manifest have been granted
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
 }
